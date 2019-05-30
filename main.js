23df16260c4d0805dd6ae956359b5171fac2c691
tallan groberg <tallan.taven@gmail.com>
2019-05-30 15:05:41 -0600

    show #winner on click

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $("#rock").on("click", function() {
+    $("#winner").show();
+  });
+  $("#paper").on("click", function() {
+    $("#winner").show();
+  });
+  $("#scissors").on("click", function() {
+    $("#winner").show();
+  });
+
+});
+
+
+// reg javascript
+
+var options = ['rock', 'paper', 'scissors']
