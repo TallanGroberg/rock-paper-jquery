38b36393a76cbf254edf7ae8420353d335c44201
tallan groberg <tallan.taven@gmail.com>
2019-05-30 16:32:32 -0600

    working game

--- main.js
+++ main.js
@@ -1,24 +1,58 @@
 $(document).ready(function(){
 
+
+
   $("#rock").on("click", function() {
+
+    $("#w").show();
+    $("#c").show();
+    $("#r").show();
     $("#winner").show();
+    if (compChoice === "paper")
+    document.getElementById('w').innerHTML = 'computer wins'
+    else if (compChoice === "scissors")
+    document.getElementById('w').innerHTML = 'player wins'
+    else
+    document.getElementById('w').innerHTML = 'well.. it\'s a tie'
+
+
+
   });
   $("#paper").on("click", function() {
+    $("#w").show();
+    $("#c").show();
+    $("#p").show();
     $("#winner").show();
+    if (compChoice === "scissors")
+    document.getElementById('w').innerHTML = 'computer wins'
+    else if (compChoice === "rock")
+    document.getElementById('w').innerHTML = 'player wins'
+    else
+    document.getElementById('w').innerHTML = 'well.. it\'s a tie'
+
   });
   $("#scissors").on("click", function() {
+    $("#w").show();
+    $("#c").show();
+    $("#s").show();
     $("#winner").show();
+    if (compChoice === "rock")
+    document.getElementById('w').innerHTML = 'computer wins'
+    else if (compChoice === "paper")
+    document.getElementById('w').innerHTML = 'player wins'
+    else
+    document.getElementById('w').innerHTML = 'well.. it\'s a tie'
+
   });
 
 });
 
-
-
-
 // reg javascript
 
 
-
 var options = ['rock', 'paper', 'scissors']
-var computer = options[Math.floor(Math.random() * options.length)];
-console.log(computer)
+var compChoice = options[Math.floor(Math.random() * options.length)];
+document.getElementById("c").innerHTML = "the computer chose " + compChoice
+document.getElementById('w').innerHTML = "the winner is..."
+
+console.log(compChoice)
