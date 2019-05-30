42b4e165bd999416e0beb54674fa7559f492da95
tallan groberg <tallan.taven@gmail.com>
2019-05-30 17:17:11 -0600

    styling

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
 
-
-
   $("#rock").on("click", function() {
 
-    $("#w").show();
-    $("#c").show();
-    $("#r").show();
-    $("#winner").show();
+    $("#w").show("#w");
+    $("#c").show("#c");
+    $("#p").show("#p");
+    $("#winner").show("#winner");
     if (compChoice === "paper")
     document.getElementById('w').innerHTML = 'computer wins'
     else if (compChoice === "scissors")
@@ -15,14 +13,12 @@ $(document).ready(function(){
     else
     document.getElementById('w').innerHTML = 'well.. it\'s a tie'
 
-
-
   });
   $("#paper").on("click", function() {
-    $("#w").show();
-    $("#c").show();
-    $("#p").show();
-    $("#winner").show();
+    $("#w").show("#w");
+    $("#c").show("#c");
+    $("#p").show("#p");
+    $("#winner").show("#winner");
     if (compChoice === "scissors")
     document.getElementById('w').innerHTML = 'computer wins'
     else if (compChoice === "rock")
@@ -32,22 +28,22 @@ $(document).ready(function(){
 
   });
   $("#scissors").on("click", function() {
-    $("#w").show();
-    $("#c").show();
-    $("#s").show();
-    $("#winner").show();
+    $("#w").show("#w");
+    $("#c").show("#c");
+    $("#p").show("#p");
+    $("#winner").show("#winner");
     if (compChoice === "rock")
     document.getElementById('w').innerHTML = 'computer wins'
     else if (compChoice === "paper")
     document.getElementById('w').innerHTML = 'player wins'
     else
     document.getElementById('w').innerHTML = 'well.. it\'s a tie'
-
   });
-
 });
 
 // reg javascript
+
+
 
 
 var options = ['rock', 'paper', 'scissors']
@@ -55,4 +51,35 @@ var compChoice = options[Math.floor(Math.random() * options.length)];
 document.getElementById("c").innerHTML = "the computer chose " + compChoice
 document.getElementById('w').innerHTML = "the winner is..."
 
-console.log(compChoice)
+// full screen method
+
+var elem = document.documentElement;
+
+/* Function to open fullscreen mode */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  }
+}
+
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+
+  }
+}
+
+// Events
+var output = document.getElementById("myP");
+document.addEventListener("fullscreenchange", function() {
+  output.innerHTML = "fullscreenchange event fired!";
+});
+
+document.addEventListener("webkitfullscreenchange", function() {
+  output.innerHTML = "webkitfullscreenchange event fired!";
+});
